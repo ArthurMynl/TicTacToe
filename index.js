ca2a027380b89dcb2623e65c433dd2b542823c42
ArthurMynl <61516506+ArthurMynl@users.noreply.github.com>
2022-06-19 21:47:03 +0200

    ajout d'un algorithme minmax - minmax ne marche pas totalement - reste à ajouter le calcul des scores - permettre de changer le jour qui commence serait intéréssant

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ newGameEl.addEventListener("click", reset);
 
 const cells = [[cell1, cell2, cell3], [cell4, cell5, cell6], [cell7, cell8, cell9]];
 let gameboard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-let isFinished = 0;
+let winner = null;
 let scoreJoueur = 0;
 let scoreOrdinateur = 0;
 
@@ -25,22 +25,9 @@ scoreJoueurEl.innerText = `Score : ${scoreJoueur}`;
 scoreOrdinateurEl.innerText = `Score : ${scoreOrdinateur}`;
 
 
-function play() {
-    for (const row of cells) {
-        for (const cell of row) {
-            cell.addEventListener("click", () => {
-                if (cell.innerHTML === "" && isFinished === 0) {
-                    cell.innerHTML = "X";
-                    gameboard[cell.id[4] - 1][cell.id[5] - 1] = "X";
-                    checkWin();
-                    if (isFinished === 0) {
-                        IA()
-                    }
-                }
-            });
-        }
-    }
-}
+const ia = 'O';
+const player = 'X';
+
 
 function reset() {
     for (const row of cells) {
@@ -49,80 +36,106 @@ function reset() {
         }
     }
     gameboard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-    isFinished = 0;
+    isFinished = false;
+    winner = null;
     winnerEl.innerText = "";
 }
 
 
+function play() {
+    for (const row of cells) {
+        for (const cell of row) {
+            cell.addEventListener("click", () => {
+                if (cell.innerHTML === "" && winner === null) {
+                    cell.innerHTML = player;
+                    gameboard[cell.id[4] - 1][cell.id[5] - 1] = player;
+                    checkWin();
+                    if (winner === null) {
+                        IAv2()
+                    }
+                }
+            });
+        }
+    }
+}
+
+
 function checkWin() {
+    if (isMoveAvailable() === false) {
+        winner = "Draw";
+    }
     for (let i = 0; i < 3; i++) {
         if (gameboard[i][0] == gameboard[i][1] && gameboard[i][1] == gameboard[i][2]) {
-            if (gameboard[i][0] == "X") {
-                isFinished = "X wins!";
-
-            } else if (gameboard[i][0] === "O") {
-                isFinished = "O wins!";
+            if (gameboard[i][0] == player) {
+                winner = player;
+            } else if (gameboard[i][0] === ia) {
+                winner = ia;
             }
         }
     }
     for (let i = 0; i < 3; i++) {
         if (gameboard[0][i] === gameboard[1][i] && gameboard[1][i] === gameboard[2][i]) {
             if (gameboard[0][i] === "X") {
-                isFinished = "X wins!";
+                winner = player;
             } else if (gameboard[0][i] === "O") {
-                isFinished = "O wins!";
+                winner = ia;
             }
         }
     }
     if (gameboard[0][0] === gameboard[1][1] && gameboard[1][1] === gameboard[2][2]) {
         if (gameboard[0][0] === "X") {
-            isFinished = "X wins!";
+            winner = player;
         } else if (gameboard[0][0] === "O") {
-            isFinished = "O wins!";
+            winner = ia;
         }
     }
     if (gameboard[0][2] === gameboard[1][1] && gameboard[1][1] === gameboard[2][0]) {
         if (gameboard[0][2] === "X") {
-            isFinished = "X wins!";
+            winner = player;
         } else if (gameboard[0][2] === "O") {
-            isFinished = "O wins!";
+            winner = ia;
         }
     }
-    var isDraw = true;
-    for (var i = 0; i < gameboard.length; i++) {
-        for (var j = 0; j < gameboard[i].length; j++) {
+    winnerEl.innerText = `${winner}`;
+}
+
+
+function countScores() {
+    if (winner !== null) {
+        winnerEl.innerText = `${winner}`;
+        if (winner === player) {
+            scoreJoueur++;
+            scoreJoueurEl.innerText = `Score : ${scoreJoueur}`;
+        }
+        else if (winner === ia) {
+            scoreOrdinateur++;
+            scoreOrdinateurEl.innerText = `Score : ${scoreOrdinateur}`;
+        }
+    }
+}
+
+
+function isMoveAvailable() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
             if (gameboard[i][j] === 0) {
-                isDraw = false;
+                return true;
             }
         }
     }
-    if (isDraw && isFinished === 0) {
-        isFinished = "Draw!";
-    }
-
-    if (isFinished !== 0) {
-        handleFinished();
-    }
+    return false;
 }
 
-function handleFinished() {
-    winnerEl.innerText = `${isFinished}`;
-    if (isFinished === "X wins!") {
-        scoreJoueur++;
-        scoreJoueurEl.innerText = `Score : ${scoreJoueur}`;
-    }
-    else if (isFinished === "O wins!") {
-        scoreOrdinateur++;
-        scoreOrdinateurEl.innerText = `Score : ${scoreOrdinateur}`;
-    }
-}
 
+/**
+ * Deprecated : in this function IA is just playing randomly
+ */
 function IA() {
     let random = Math.floor(Math.random() * 3);
     let random2 = Math.floor(Math.random() * 3);
     if (gameboard[random][random2] === 0) {
-        cells[random][random2].innerHTML = "O";
-        gameboard[random][random2] = "O";
+        cells[random][random2].innerHTML = ia;
+        gameboard[random][random2] = ia;
         checkWin();
     }
     else {
@@ -130,44 +143,73 @@ function IA() {
     }
 }
 
+function IAv2() {
+    let bestMove = findBestMove();
+    cells[bestMove[0]][bestMove[1]].innerHTML = ia;
+    gameboard[bestMove[0]][bestMove[1]] = ia;
+    checkWin();
+}
 
-function minmax(node, depth, maximizingPlayer) {
-    if (checkWin() !== 0) {
-        if (checkWin() === "X wins!") {
-            return -10 + depth;
-        }
-        else if (checkWin() === "O wins!") {
-            return 10 - depth;
-        }
-        else {
-            return 0;
-        }
-    }
-    if (maximizingPlayer) {
-        let best = -Infinity;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (gameboard[i][j] === 0) {
-                    gameboard[i][j] = "X";
-                    best = Math.max(best, minmax(node, depth + 1, false));
-                    gameboard[i][j] = 0;
+function findBestMove() {
+    let bestScore = -Infinity;
+    let bestMove;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (gameboard[i][j] === 0) {
+                gameboard[i][j] = ia;
+                let score = minmax(gameboard, 0, false);
+                winner = null;
+                gameboard[i][j] = 0;
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestMove = [i, j];
                 }
             }
         }
-        return best;
+    }
+    return bestMove;
+}
+
+
+
+function minmax(gameboard, depth, isMaximizing) {
+    checkWin();
+    if (winner === ia) {
+        return 10 - depth;
+    }
+    else if (winner === player) {
+        return -10 + depth;
+    }
+    else if (winner === "Draw") {
+        return 0;
+    }
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (gameboard[i][j] === 0) {
+                    gameboard[i][j] = ia;
+                    let score = minmax(gameboard, depth + 1, false);
+                    gameboard[i][j] = 0;
+                    bestScore = Math.max(bestScore, score);
+                }
+            }
+        }
+        return bestScore;
     }
     else {
-        let best = Infinity;
+        let bestScore = Infinity;
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if (gameboard[i][j] === 0) {
-                    gameboard[i][j] = "O";
-                    best = Math.min(best, minmax(node, depth + 1, true));
+                    gameboard[i][j] = player;
+                    let score = minmax(gameboard, depth + 1, true);
                     gameboard[i][j] = 0;
+                    bestScore = Math.min(bestScore, score);
                 }
             }
         }
-        return best;
+        return bestScore;
     }
 }
 
